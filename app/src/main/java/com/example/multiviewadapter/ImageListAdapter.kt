0c3067de55cfe403253768d5907ea6f55e8b7015
fawad.ali <fawad.ali@appinSnap.com>
2023-07-18 01:51:57 +0500

    init Commit

--- app/src/main/java/com/example/multiviewadapter/ImageListAdapter.kt
+++ app/src/main/java/com/example/multiviewadapter/ImageListAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+
 class ImageListAdapter(private val imageList: List<String>,
                        private val onItemClick: (Int) -> Unit
                        ) :
